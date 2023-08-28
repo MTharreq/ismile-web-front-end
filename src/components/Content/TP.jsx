040351d21bab23f252bea87ee163649332dec22a
HimejiSoul <perseushilmy@gmail.com>
2023-08-29 02:06:44 +0700

    rapihin content

--- src/components/Content/TP.jsx
+++ src/components/Content/TP.jsx
@@ -1,95 +1,15 @@
+import React from 'react';
+import ModuleCard from './ModuleCard';
+
 export default function TP() {
     return (
-        <div className="w-full h-full flex flex-col gap-4">
-        {/* MODUL 1 */}
-        <div className="flex bg-white py-4 px-6 rounded-lg shadow-md justify-between items-center">
-        <div className="flex flex-col w-fit">
-            <h2 className="text-base">MODUL 1 - Searching</h2>
-            <p className="text-sm font-normal italic text-[#A3A7A8]">Last edited: Sat, 13 July 19:20</p>
-        </div>
-        <div className="flex w-fit gap-4">
-            <div className="text-sm font-bold text-slate-800 text-md flex self-center">
-            Closed
-            <span className="material-symbols-rounded align-middle ml-2">lock</span>
-            </div>
-            <div className="flex justify-center m-auto py-3 px-4 bg-[#4DD7BE] text-white rounded-lg shadow-md cursor-pointer">Open</div>
-        </div>
-        </div>
-
-        {/* MODUL 2 */}
-        <div className="flex bg-white py-4 px-6 rounded-lg shadow-md justify-between items-center">
-        <div className="flex flex-col w-fit">
-            <h2 className="text-lg">MODUL 2 - Knowledge Representation</h2>
-            <p className="text-sm font-normal italic text-[#A3A7A8]">Last edited: Sat, 13 July 19:20</p>
-        </div>
-        <div className="flex w-fit gap-4">
-            <div className="font-bold rounded-lg text-slate-800 text-md flex self-center">
-            Closed
-            <span className="material-symbols-rounded align-middle ml-2">lock</span>
-            </div>
-            <div className="flex justify-center m-auto py-3 px-4 bg-[#4DD7BE] text-white rounded-lg shadow-md cursor-pointer">Open</div>
-        </div>
-        </div>
-
-        {/* MODUL 3 */}
-        <div className="flex bg-white py-4 px-6 rounded-lg shadow-md justify-between items-center">
-        <div className="flex flex-col w-fit">
-            <h2 className="text-lg">MODUL 3 - Fuzzyfikasi</h2>
-            <p className="text-sm font-normal italic text-[#A3A7A8]">Last edited: Sat, 13 July 19:20</p>
-        </div>
-        <div className="flex w-fit gap-4">
-            <div className="font-bold rounded-lg text-slate-800 text-md flex self-center">
-            Closed
-            <span className="material-symbols-rounded align-middle ml-2">lock</span>
-            </div>
-            <div className="flex justify-center m-auto py-3 px-4 bg-[#4DD7BE] text-white rounded-lg shadow-md cursor-pointer">Open</div>
-        </div>
-        </div>
-
-        {/* MODUL 4 */}
-        <div className="flex bg-white py-4 px-6 rounded-lg shadow-md justify-between items-center">
-        <div className="flex flex-col w-fit">
-            <h2 className="text-lg">MODUL 4 - Fuzzyfikasi: Mamdani</h2>
-            <p className="text-sm font-normal italic text-[#A3A7A8]">Last edited: Sat, 13 July 19:20</p>
-        </div>
-        <div className="flex w-fit gap-4">
-            <div className="font-bold rounded-lg text-slate-800 text-md flex self-center">
-            Closed
-            <span className="material-symbols-rounded align-middle ml-2">lock</span>
-            </div>
-            <div className="flex justify-center m-auto py-3 px-4 bg-[#4DD7BE] text-white rounded-lg shadow-md cursor-pointer">Open</div>
-        </div>
-        </div>
-
-        {/* MODUL 5 */}
-        <div className="flex bg-white py-4 px-6 rounded-lg shadow-md justify-between items-center">
-        <div className="flex flex-col w-fit">
-            <h2 className="text-lg">MODUL 5 - Fuzzyfikasi: Sugeno</h2>
-            <p className="text-sm font-normal italic text-[#A3A7A8]">Last edited: Sat, 13 July 19:20</p>
-        </div>
-        <div className="flex w-fit gap-4">
-            <div className="font-bold rounded-lg text-slate-800 text-md flex self-center">
-            Closed
-            <span className="material-symbols-rounded align-middle ml-2">lock</span>
-            </div>
-            <div className="flex justify-center m-auto py-3 px-4 bg-[#4DD7BE] text-white rounded-lg shadow-md cursor-pointer">Open</div>
-        </div>
-        </div>
-
-        {/* MODUL 6 */}
-        <div className="flex bg-white py-4 px-6 rounded-lg shadow-md justify-between items-center">
-        <div className="flex flex-col w-fit">
-            <h2 className="text-lg">MODUL 6 - Algoritma Genetika</h2>
-            <p className="text-sm font-normal italic text-[#A3A7A8]">Last edited: Sat, 13 July 19:20</p>
-        </div>
-        <div className="flex w-fit gap-4">
-            <div className="font-bold rounded-lg text-slate-800 text-md flex self-center">
-            Closed
-            <span className="material-symbols-rounded align-middle ml-2">lock</span>
-            </div>
-            <div className="flex justify-center m-auto py-3 px-4 bg-[#4DD7BE] text-white rounded-lg shadow-md cursor-pointer">Open</div>
-        </div>
-        </div>
+    <div className="w-full h-full flex flex-col gap-4">
+        <ModuleCard title="MODUL 1 - Searching" status="Open" />
+        <ModuleCard title="MODUL 2 - Knowledge Representation" status="Open" />
+        <ModuleCard title="MODUL 3 - Fuzzyfikasi" status="Open" />
+        <ModuleCard title="MODUL 4 - Fuzzyfikasi: Mamdani" status="Open" />
+        <ModuleCard title="MODUL 5 - Fuzzyfikasi: Sugeno" status="Open" />
+        <ModuleCard title="MODUL 6 - Algoritma Genetika" status="Open" />
     </div>
-    )
+    );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
 export default function Sidebar() {
     return (
-        <div className="bg-white h-screen w-96">
+        <div className="bg-white h-screen w-[300px]">
             <img className="w-1/2 bg-white ml-6 mt-6 mb-6" src="src\assets\logo\Horizontal.jpg" alt="i-Smile Logo" />
             
             {/* LIST-BUTTON */}
             <div className="flex flex-col gap-4 py-4">
-                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 text-md cursor-pointer">
-                    <span className="material-symbols-rounded align-middle">library_books</span>
-                    <span className="pl-2">Tugas Pendahuluan</span>
+                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 text-sm cursor-pointer">
+                    <span className="material-symbols-rounded align-middle pr-2">library_books</span>
+                    Tugas Pendahuluan
                 </div>
                 <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 text-md cursor-pointer">
                     <span className="material-symbols-rounded align-middle">library_books</span>
