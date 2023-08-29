2fe62648a69af182370ad61f3e1f94bf8285a42c
MTharreq <mtharreq@gmail.com>
2023-08-29 10:20:00 +0700

    Sidebar Changes

--- src/components/Content/TP.jsx
+++ src/components/Content/TP.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ModuleCard from './ModuleCard';
 
 export default function TP() {
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from 'react-router-dom'
+// import React from "react"
+// import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
     return (
@@ -7,28 +7,26 @@ export default function Sidebar() {
             <img className="w-1/2 bg-white ml-6 mt-6 mb-6" src="src\assets\logo\Horizontal.jpg" alt="i-Smile Logo" />
 
             {/* LIST-BUTTON */}
-            <div className="flex flex-col gap-4 py-4">
-                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 text-md cursor-pointer">
-                    <span className="material-symbols-rounded align-middle">library_books</span>
-                    <span className="pl-2">Tugas Pendahuluan</span>
+            <div className="flex flex-col gap-4 py-4 ">
+                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 flex gap-2 text-sm items-center active:bg-[#4DD7BE] cursor-pointer">
+                    <span className="material-symbols-rounded">library_books</span>
+                    Tugas Pendahuluan
                 </div>
-                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 text-md cursor-pointer">
-                    <span className="material-symbols-rounded align-middle">library_books</span>
-                    <span className="pl-2">Tes Awal</span>
+                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 flex gap-2 text-sm items-center active:bg-[#4DD7BE] cursor-pointer">
+                    <span className="material-symbols-rounded">library_books</span>
+                    Tes Awal
                 </div>
-                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 text-md cursor-pointer">
-                    <span className="material-symbols-rounded align-middle">laptop_chromebook</span>
-                    <span className="pl-2">Jurnal</span>
+                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 flex gap-2 text-sm items-center active:bg-[#4DD7BE] cursor-pointer">
+                    <span className="material-symbols-rounded">laptop_chromebook</span>
+                    Jurnal
                 </div>
-                
-                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 text-md cursor-pointer">
-                    <span className="material-symbols-rounded align-middle">slideshow</span>
-                    <span className="pl-2">Modul Praktikum</span>
+                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 flex gap-2 text-sm items-center active:bg-[#4DD7BE] cursor-pointer">
+                    <span className="material-symbols-rounded">slideshow</span>
+                    Modul Praktikum
                 </div>
-                
-                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 text-md cursor-pointer">
-                    <span className="material-symbols-rounded align-middle">search</span>
-                    <span className="pl-2">About i-Smile 8</span>
+                <div className="transition font-bold bg-white hover:bg-[#4DD7BE] hover:text-white hover:shadow-md mx-4 px-4 py-3 rounded-lg text-slate-800 flex gap-2 text-sm items-center active:bg-[#4DD7BE] cursor-pointer">
+                    <span className="material-symbols-rounded">search</span>
+                    About i-Smile 8
                 </div>
             
             </div>
