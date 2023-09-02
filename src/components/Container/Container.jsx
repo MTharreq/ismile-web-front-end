847564b97819aa626d66a520640a8933e4d1f18d
MTharreq <mtharreq@gmail.com>
2023-09-02 15:35:31 +0700

    Responsive Webs 0.0.1

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,15 +1,23 @@
-import AppBar from "../AppBar/AppBar"
-import TP from "../Content/TP"
-import TA from "../Content/TA"
-import About from "../Content/About"
+/* eslint-disable react/prop-types */
+import { Route, Routes } from 'react-router-dom';
+import AppBar from "../AppBar/AppBar";
+import About from "../Content/About";
+import TP from "../Content/TP";
+import TA from "../Content/TA";
+import Jurnal from "../Content/Jurnal";
 
-export default function Container() {
+
+export default function Container({isSidebarOpen, setIsSidebarOpen}) {
   return (
-    <div className="flex flex-col bg-[#F2F2F2] w-full font-bold p-8 gap-8">
-      <AppBar />
-      <TP />
-      {/* <TA /> */}
-      {/* <About /> */}
+    <div className={`flex flex-col bg-[#F2F2F2] h-screen font-bold gap-4 p-4  ${isSidebarOpen ? '':'sm:ml-64'}`}>
+    {/* <div className="flex flex-col bg-[#F2F2F2] h-screen font-bold gap-4 p-4"> */}
+      <AppBar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
+      <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/tugas-pendahuluan" element={<TP />} />
+          <Route path="/tugas-awal" element={<TA />} />
+          <Route path="/jurnal" element={<Jurnal />} />
+      </Routes>
     </div>
   )
 }
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Sidebar from "../components/Sidebar/Sidebar";
 import Container from "../components/Container/Container";
-import AppBar from "../components/AppBar/AppBar";
-import TP from "../components/Content/TP";
-import TA from "../components/Content/TA";
-import About from "../components/Content/About";
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+    let menuRef = useRef()
+
+    useEffect(() => {
+        let handler = (e) => {
+            if(!menuRef.current.contains(e.target)){
+                setIsSidebarOpen(false)
+                console.log(isSidebarOpen)
+            }
+        }
+
+        document.addEventListener('mousedown', handler)
+
+        return() => {
+            document.removeEventListener('mousedown', handler)
+        }
+
+    })
+
     return (
         <BrowserRouter>
-            <div className="flex flex-row">
-                <Sidebar />
-                <div className="flex flex-col bg-[#F2F2F2] w-full font-bold p-8 gap-8">
-                    <AppBar />
-                    <Routes>
-                        <Route path="/" element={<About />} />
-                        <Route path="/tugas-pendahuluan" element={<TP />} />
-                        <Route path="/tugas-awal" element={<TA />} />
-                    </Routes>
-                </div>
-            </div>
+        <div ref={menuRef}>
+            <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+            <Container isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+        </div>
         </BrowserRouter>
     )
 }
